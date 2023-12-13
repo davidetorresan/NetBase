9b10d3035f561fda4c9169269daf9a53df04bac2
Davide Torresan <info@davidetorresan.it>
2023-12-13 22:12:53 +0100

    commit

--- app/(dashboard)/(routes)/notes/new/page.tsx
+++ app/(dashboard)/(routes)/notes/new/page.tsx
@@ -11,10 +11,12 @@ import axios from "axios";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+import Select from "react-select";
+
 const NewNotesPage = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const [text, setText] = useState();
-  const onEditorStateChange = function (editorState:any) {
+  const onEditorStateChange = function (editorState: any) {
     setEditorState(editorState);
     const { blocks } = convertToRaw(editorState.getCurrentContent());
     let text = editorState.getCurrentContent().getPlainText("\u0001");
@@ -22,10 +24,15 @@ const NewNotesPage = () => {
   };
 
   async function handleSubmit() {
-    let data = {}
-    let res = await axios.post("/api/notes", data)
+    let data = {};
+    let res = await axios.post("/api/notes", data);
   }
 
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
 
   return (
     <div>
@@ -39,7 +46,10 @@ const NewNotesPage = () => {
       <div className="px-4 lg:px-8">
         <div className="rounded-lg border p-4 px-3 md:px-6 focus-within:shadow-sm w-full">
           <h3 className="font-bold mb-1">Titolo</h3>
-          <Input className="mb-4" placeholder="Inserisci il titolo della nota"/>
+          <Input
+            className="mb-4"
+            placeholder="Inserisci il titolo della nota"
+          />
           <h3 className="font-bold mb-1">Contenuto</h3>
           <div className="rounded-lg border p-4 px-3 md:px-6 focus-within:shadow-sm w-full mb-4">
             <Editor
@@ -50,9 +60,22 @@ const NewNotesPage = () => {
               onEditorStateChange={onEditorStateChange}
             />
           </div>
-        <Button onClick={() => handleSubmit} variant="default" className="w-full">
-          Save
-        </Button>
+          <h3 className="font-bold mb-1">Categorie</h3>
+          <Select
+            defaultValue={[options[2], options[3]]}
+            isMulti
+            name="colors"
+            options={options}
+            className="basic-multi-select w-1/4 mb-4"
+            classNamePrefix="select"
+          />
+          <Button
+            onClick={() => handleSubmit}
+            variant="default"
+            className="w-full"
+          >
+            Save
+          </Button>
         </div>
       </div>
       <div className="px-4 lg:px-8 space-y-4 mt-10"></div>
